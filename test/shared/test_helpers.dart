70a3d0541e91351803e794fcfec73dcdc3f72d95
reaferon <reaferon@ya.ru>
2023-03-12 22:44:29 +0300

    tests 3

--- test/shared/test_helpers.dart
+++ test/shared/test_helpers.dart
@@ -1,4 +1,16 @@
+import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
+
+Finder findTypeByTextOnlyInParentType(
+  final Type type,
+  final String text,
+  final Type parentType,
+) {
+  return find.descendant(
+    of: find.byType(parentType),
+    matching: find.ancestor(of: find.text(text), matching: find.byType(type)),
+  );
+}
 
 Matcher isOneOrAnother(dynamic one, dynamic another) => OneOrAnotherMatcher(one, another);
 
@@ -16,4 +28,13 @@ class OneOrAnotherMatcher extends Matcher {
 
   @override
   bool matches(Object? item, Map matchState) => item == _one || item == _another;
+}
+
+extension MyIterable<T> on Iterable<T> {
+  T? firstWhereOrNull(bool Function(T element) test) {
+    for (var element in this) {
+      if (test(element)) return element;
+    }
+    return null;
+  }
 }
